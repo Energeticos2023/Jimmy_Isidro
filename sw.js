1957e3fcc7d9dfafd9dbaf6bcffa3f9fc73867ed
Club Rotary Huaraz Colonial <jose.zena.p@uni.pe>
2026-08-08 21:27:45 -0500

    Actualiza la caché de la PWA para el nuevo encuadre móvil

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "jimmy-isidro-v5.0.0";
+const CACHE = "jimmy-isidro-v5.0.1";
 const APP_SHELL = [
   "./", "./index.html", "./styles.css", "./app.js", "./config.js", "./data.js",
   "./manifest.webmanifest",
