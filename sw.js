da152b1bff60cc08e1ab9572a56144dd45d367e7
Club Rotary Huaraz Colonial <jose.zena.p@uni.pe>
2026-08-08 21:43:25 -0500

    Actualiza la PWA para la campaña de visitas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "jimmy-isidro-v5.0.2";
+const CACHE = "jimmy-isidro-v5.0.3";
 const APP_SHELL = [
   "./", "./index.html", "./styles.css", "./app.js", "./config.js", "./data.js",
   "./manifest.webmanifest",
