70d76edf7ffa9a60d69783daecd1c45fcf218f22
Club Rotary Huaraz Colonial <jose.zena.p@uni.pe>
2026-08-08 21:31:49 -0500

    Agrega el enlace del grupo oficial de WhatsApp

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ window.APP_CONFIG = {
   appSubtitle: "Experiencia que construye · Juventud que transforma",
   facebookUrl: "https://www.facebook.com/Jimmyisidrovillanueva1",
   whatsappNumber: "51981918440",
+  whatsappGroupUrl: "https://chat.whatsapp.com/DDWymgvkl5TL8M90vB8G16",
   whatsappText: "Hola, quiero sumarme y conocer más sobre Jimmy Isidro y sus propuestas para Independencia.",
   forms: {
     personeros: "https://forms.cloud.microsoft/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAANAAqIKWlRUMktGUzNPOUlDNU81RFQ0UFdUOTFZTEo4MC4u",
